50c4081665bd4c682ea81040c5e80f9b04d6a5ed
Ian Woodard <ianwoodard2000@gmail.com>
2020-12-18 09:40:45 -0500

    Added firebase to add new users to db

--- js/app.js
+++ js/app.js
@@ -37,3 +37,59 @@ function logOutGoogle() {
     });
   return false;
 }
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    if (window.localStorage.getItem("hasAccount") == null) {
+      addUserIfNew(user);
+    }
+    window.localStorage.setItem("expectedLoggedIn", true);
+    $("#login").hide();
+    $("#profile").show();
+    $("#logout").show();
+    $(".nav.visible").height("250px");
+    $("#nav").addClass("logged-in");
+  } else {
+    window.localStorage.removeItem("expectedLoggedIn");
+    $("#profile").hide();
+    $("#logout").hide();
+    $("#login").show();
+    $("#nav").removeClass("logged-in");
+  }
+});
+
+function addUserIfNew(user) {
+  var ref = firebase.database().ref("users/" + user.uid);
+  ref.once("value", (snapshot) => {
+    const data = snapshot.val();
+    if (data == null) {
+      addUserDefault(user);
+    }
+  });
+}
+
+function addUserDefault(user) {
+  var ref = firebase.database().ref("users/" + user.uid);
+  var user_name = user.displayName;
+  ref.set({
+    name: user_name,
+  });
+  var date_joined = new Date(user.metadata.creationTime);
+  var date_joined_ref = ref.child("joined");
+  date_joined_ref.set({
+    date: date_joined.toLocaleDateString(),
+  });
+  var photo_url = user.photoURL;
+  var photo_url_ref = ref.child("photo");
+  photo_url_ref.set({
+    url: photo_url,
+  });
+  var uid_val = user.uid;
+  var id_ref = ref.child("id");
+  id_ref.set({
+    uid: uid_val,
+  });
+  var num_solved_ref = ref.child("numSolved");
+  num_solved_ref.set({
+    count: 0,
+  });
+}
