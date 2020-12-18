c2287421714a744dcbb7d13d0f99b7c7335ae114
Ian Woodard <ianwoodard2000@gmail.com>
2020-12-18 09:26:40 -0500

    Added navbar

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDSdhny1Hh7xBShHXKPhHGT4TzbVhIKXz4",
-    authDomain: "code-practice-20d1f.firebaseapp.com",
-    databaseURL: "https://code-practice-20d1f-default-rtdb.firebaseio.com",
-    projectId: "code-practice-20d1f",
-    storageBucket: "code-practice-20d1f.appspot.com",
-    messagingSenderId: "597732026547",
-    appId: "1:597732026547:web:a2449919ae0399c19aa8c6",
-    measurementId: "G-FE1K5LDBDE"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  apiKey: "AIzaSyDSdhny1Hh7xBShHXKPhHGT4TzbVhIKXz4",
+  authDomain: "code-practice-20d1f.firebaseapp.com",
+  databaseURL: "https://code-practice-20d1f-default-rtdb.firebaseio.com",
+  projectId: "code-practice-20d1f",
+  storageBucket: "code-practice-20d1f.appspot.com",
+  messagingSenderId: "597732026547",
+  appId: "1:597732026547:web:a2449919ae0399c19aa8c6",
+  measurementId: "G-FE1K5LDBDE",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
