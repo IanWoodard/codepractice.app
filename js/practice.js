573402cacb8f56992b845c63db1bd40a18fa872a
Ian Woodard <ianwoodard2000@gmail.com>
2020-12-26 12:49:32 -0600

    attempt 2 at getting bookmarks working

--- js/practice.js
+++ js/practice.js
@@ -9,49 +9,47 @@ $("#show-less").click(function () {
   $("#show-more").show();
 });
 $("#bookmark").click(function () {
-  var user = firebase.auth().currentUser;
+  var cur_user = firebase.auth().currentUser;
   var path = window.location.pathname;
   var problem = path.substring(1, path.indexOf("."));
-  if (user) {
+  if (cur_user) {
     if ($("#bookmark").attr("src") == "svg/bookmark.svg") {
       $("#bookmark").attr("src", "svg/bookmarked.svg");
       firebase
         .database()
-        .ref("users/" + user.uid + "/bookmarks/" + problem)
+        .ref("users/" + cur_user.uid + "/bookmarks/" + problem)
         .set({ bookmarked: true });
     } else {
       $("#bookmark").attr("src", "svg/bookmark.svg");
       firebase
         .database()
-        .ref("users/" + user.uid + "/bookmarks")
+        .ref("users/" + cur_user.uid + "/bookmarks")
         .child(problem)
         .remove();
     }
   }
 });
-$(document).ready(function () {
-  var user = firebase.auth().currentUser;
-  var path = window.location.pathname;
-  var problem = path.substring(1, path.indexOf("."));
-  if (user) {
-    if (window.localStorage.getItem(problem + "-bookmark") != undefined) {
-      $("#bookmark").attr("src", "svg/bookmarked.svg");
-    }
-    firebase
-      .database()
-      .ref("users/" + user.uid + "/bookmarks/" + problem)
-      .once("value", (snapshot) => {
-        if (snapshot.val() != null) {
-          window.localStorage.setItem(problem + "-bookmark");
-          if ($("#bookmark").attr("src") == "svg/bookmark.svg") {
-            $("#bookmark").attr("src", "svg/bookmarked.svg");
-          }
-        } else {
-          window.localStorage.removeItem(problem + "-bookmark");
-          if ($("#bookmark").attr("src") != "svg/bookmark.svg") {
-            $("#bookmark").attr("src", "svg/bookmark.svg");
-          }
-        }
-      });
+var user = firebase.auth().currentUser;
+var path = window.location.pathname;
+var problem = path.substring(1, path.indexOf("."));
+if (user) {
+  if (window.localStorage.getItem(problem + "-bookmark") != undefined) {
+    $("#bookmark").attr("src", "svg/bookmarked.svg");
   }
-});
+  firebase
+    .database()
+    .ref("users/" + user.uid + "/bookmarks/" + problem)
+    .once("value", (snapshot) => {
+      if (snapshot.val() != null) {
+        window.localStorage.setItem(problem + "-bookmark");
+        if ($("#bookmark").attr("src") == "svg/bookmark.svg") {
+          $("#bookmark").attr("src", "svg/bookmarked.svg");
+        }
+      } else {
+        window.localStorage.removeItem(problem + "-bookmark");
+        if ($("#bookmark").attr("src") != "svg/bookmark.svg") {
+          $("#bookmark").attr("src", "svg/bookmark.svg");
+        }
+      }
+    });
+}
