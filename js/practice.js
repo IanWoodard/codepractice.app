b9ae420ad7bf465fcfbe91da68c8a91a531a11d4
Ian Woodard <ianwoodard2000@gmail.com>
2020-12-26 13:00:45 -0600

    Changed preload to its own file and fixed bookmark

--- js/practice.js
+++ js/practice.js
@@ -1,57 +1,45 @@
-$("#show-more").click(function () {
-  $("#show-more").hide();
-  $("#documentation").addClass("expanded");
-  $("#show-less").show();
-});
-$("#show-less").click(function () {
-  $("#show-less").hide();
-  $("#documentation").removeClass("expanded");
-  $("#show-more").show();
-});
 $("#bookmark").click(function () {
   var cur_user = firebase.auth().currentUser;
-  var path = window.location.pathname;
-  var problem = path.substring(1, path.indexOf("."));
+  var cur_path = window.location.pathname;
+  var cur_problem = cur_path.substring(1, cur_path.indexOf("."));
   if (cur_user) {
     if ($("#bookmark").attr("src") == "svg/bookmark.svg") {
       $("#bookmark").attr("src", "svg/bookmarked.svg");
       firebase
         .database()
-        .ref("users/" + cur_user.uid + "/bookmarks/" + problem)
+        .ref("users/" + cur_user.uid + "/bookmarks/" + cur_problem)
         .set({ bookmarked: true });
     } else {
       $("#bookmark").attr("src", "svg/bookmark.svg");
       firebase
         .database()
         .ref("users/" + cur_user.uid + "/bookmarks")
-        .child(problem)
+        .child(cur_problem)
         .remove();
     }
   }
 });
-$(document).ready(function () {
-  var user = firebase.auth().currentUser;
-  var path = window.location.pathname;
-  var problem = path.substring(1, path.indexOf("."));
-  if (user) {
-    if (window.localStorage.getItem(problem + "-bookmark") != undefined) {
-      $("#bookmark").attr("src", "svg/bookmarked.svg");
-    }
-    firebase
-      .database()
-      .ref("users/" + user.uid + "/bookmarks/" + problem)
-      .once("value", (snapshot) => {
-        if (snapshot.val() != null) {
-          window.localStorage.setItem(problem + "-bookmark");
-          if ($("#bookmark").attr("src") == "svg/bookmark.svg") {
-            $("#bookmark").attr("src", "svg/bookmarked.svg");
-          }
-        } else {
-          window.localStorage.removeItem(problem + "-bookmark");
-          if ($("#bookmark").attr("src") != "svg/bookmark.svg") {
-            $("#bookmark").attr("src", "svg/bookmark.svg");
-          }
-        }
-      });
+var user = firebase.auth().currentUser;
+var path = window.location.pathname;
+var problem = path.substring(1, path.indexOf("."));
+if (user) {
+  if (window.localStorage.getItem(problem + "-bookmark") != undefined) {
+    $("#bookmark").attr("src", "svg/bookmarked.svg");
   }
-});
+  firebase
+    .database()
+    .ref("users/" + user.uid + "/bookmarks/" + problem)
+    .once("value", (snapshot) => {
+      if (snapshot.val() != null) {
+        window.localStorage.setItem(problem + "-bookmark");
+        if ($("#bookmark").attr("src") == "svg/bookmark.svg") {
+          $("#bookmark").attr("src", "svg/bookmarked.svg");
+        }
+      } else {
+        window.localStorage.removeItem(problem + "-bookmark");
+        if ($("#bookmark").attr("src") != "svg/bookmark.svg") {
+          $("#bookmark").attr("src", "svg/bookmark.svg");
+        }
+      }
+    });
+}
