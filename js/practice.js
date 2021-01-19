5339119cc390ef3e82812c3ebac3ddaf64e37eb1
Ian Woodard <ianwoodard2000@gmail.com>
2021-01-19 10:30:49 -0600

    Attempting to fix paths v2

--- js/practice.js
+++ js/practice.js
@@ -1,7 +1,8 @@
 $("#bookmark").click(function () {
   var cur_user = firebase.auth().currentUser;
   var cur_path = window.location.pathname;
-  var cur_problem = cur_path.includes(".") ? cur_path.substring(1, cur_path.indexOf(".")) : cur_path.substring(1);
+  var cur_problem = cur_path.replace("/problems/", "");
+  cur_problem = cur_problem.replace("/", "");
   if (cur_user) {
     if ($("#bookmark").attr("src") == "../../svg/bookmark.svg") {
       $("#bookmark").attr("src", "../../svg/bookmarked.svg");
